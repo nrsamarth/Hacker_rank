fdb430919723e5f35473b3868e57b1659a801d95
nrsamarth <122877460+nrsamarth@users.noreply.github.com>
2024-06-11 09:14:09 +0530

    Create hello withtab.c

---
+++ hello withtab.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello World\tHello World\n");
+    return 0;
+}
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    
