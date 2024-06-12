eb89a04c0a535728835e65d071687c0fd788f1f6
nrsamarth <122877460+nrsamarth@users.noreply.github.com>
2024-06-12 09:15:31 +0530

    Create ascii2.c

---
+++ ascii2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int asciiValue;
+    
+    // Read an integer (ASCII value) from input
+    scanf("%d", &asciiValue);
+    
+    // Print the corresponding character
+    printf("%c\n", asciiValue);
+
+    return 0;
+}
